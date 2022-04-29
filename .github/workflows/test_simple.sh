bed0e7ccc91dc403f45188b45c82a224425dd7e7
Markus Neteler <neteler@gmail.com>
2022-04-29 20:05:08 +0200

    use /home/mneteler/gdalplugins for testing

--- .github/workflows/test_simple.sh
+++ .github/workflows/test_simple.sh
@@ -6,8 +6,16 @@ set -e
 # print commands
 set -x
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 GDAL_AUTOLOAD_DIR"
+    exit 1
+fi
+
 # non-existent variables as an errors
 set -u
+
+export GDAL_AUTOLOAD_DIR=$1
+export GDAL_DRIVER_PATH=${GDAL_AUTOLOAD_DIR}
 
 # add small GRASS GIS dataset for tests
 (mkdir -p $HOME/grassdata && \
